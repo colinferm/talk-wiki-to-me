7612f223dad8f90c1328333b1163cd183bea2a23
Colin Ferm <colin@fortytwosolutions.com>
2016-05-22 14:36:24 -0400

    Added caching to page summaries.

--- plugin.php
+++ plugin.php
@@ -30,7 +30,10 @@ class talkwikitome {
      *
      */
     function talkwikitomeTranslate($content) {
-        global $wpdb;
+        global $wpdb, $post;
+        error_log("Post ID? ".$post->ID);
+        $existing_tags = get_post_meta($post->ID, 'talkwikitome_tags', true);
+        $summaries = get_post_meta($post->ID, 'talkwikitome_wiki_summaries', true);
 
         // TODO: This should simply be a global variable.
         $db_linktable = $wpdb->prefix . "talkwikitome_links";
@@ -56,63 +59,76 @@ class talkwikitome {
         // Look to see if the pattern is a match: (url)|(term)|(text)
 
         $match = array();
-		$refBullets = array();
-		$tags = array();
+	$refBullets = array();
+	$tags = array();
         $position = 0;
 
         // Continue to look through the content for all the matchnes, moving forward
         //  and keeping track of the position...
         while (($position = preg_match($pattern, $content, $match, 0, $position))) {
+                $tag = $wpdb->escape( $match[1] );
+                $term = urlencode(str_replace(' ', '_', $match[2]));
+                $text = $match[3];
 
-            $tag = $wpdb->escape( $match[1] );
-            $term = urlencode(str_replace(' ', '_', $match[2]));
-            $text = $match[3];
+                if (!strlen($text)) $text = $term;
 
-	    	if (!strlen($text)) $text = $term;
-
-            $link = $wpdb->get_row ( "SELECT * FROM $db_linktable WHERE tag='$tag' " );
+                $link = $wpdb->get_row ( "SELECT * FROM $db_linktable WHERE tag='$tag' " );
+                $url = $link->url . str_replace(' ', '_', $match[2]);
+                $anchor = "<a href=\"$url\" ";
             
-            // TODO: Allow a way to designate the search term within the url
-            $url = $link->url . str_replace(' ', '_', $match[2]);
+                if ( $link->follow == 1 )  {
+                        $anchor .= " nofollow ";
+                }
 
-            $anchor = "<a href=\"$url\" ";
-            
-            if ( $link->follow == 1 )  {
-                $anchor .= " nofollow ";
-            }
+                if ( $link->window == 0 )  {
+                        $anchor .= "target=\"_blank\" ";
+                } else {
+                        $anchor .= "target=\"_top\" ";
+                }
 
-            if ( $link->window == 0 )  {
-                $anchor .= "target=\"_blank\" ";
-            } else {
-                $anchor .= "target=\"_top\" ";
-            }
+                if ( $link->alt_attribute == 0 )  {
+                        $anchor .= "alt=\"$text\" ";
+                }
 
-            if ( $link->alt_attribute == 0 )  {
-                $anchor .= "alt=\"$text\" ";
-            }
+                if ( $link->title_attribute == 0 )  {
+                        $anchor .= "title=\"$text\" ";
+                }
 
-            if ( $link->title_attribute == 0 )  {
-                $anchor .= "title=\"$text\" ";
-            }
+                if ( $link->style_override != null )  {
+                        $anchor .= "style=\"" . $link->style_override . "\" ";
+                }
 
-            if ( $link->style_override != null )  {
-                $anchor .= "style=\"" . $link->style_override . "\" ";
-            }
+                $ref = array(
+                        'link' => $anchor,
+                        'text' => $text
+                );
+                array_push($refBullets, $ref);
+                array_push($tags, array(
+                        'position' => $position,
+                        'tag' => $term
+                ));
 
-		    $ref = array(
-				'link' => $anchor,
-				'text' => $text
-		    );
-		    array_push($refBullets, $ref);
-		    array_push($tags, array(
-		    	'position' => $position,
-		    	'tag' => $term
-		    ));
+                $content = str_replace($match[0],$anchor . ">$text</a>",$content);
+        }
 
-            $content = str_replace($match[0],$anchor . ">$text</a>",$content);
+        $check_new_data = false;
+        if (count($tags) > 0) {
+                if (count($summaries) == 0) {
+                        error_log('No summaries, should fetch...');
+                        $check_new_data = true;
+                }
+                $diff = array_diff_assoc($existing_tags, $tags);
+                if (count($diff)) {
+                        update_post_meta($post->ID, 'talkwikitome_tags', $tags);
+                        $check_new_data = true;
+                        error_log('There looks to be new tags.');
+                        error_log('Old num: '.count($existing_tags).', new num: '.count($tags));
+                }
         }
         
-        if (count($tags)) {
+        // For debug
+        //$check_new_data = true;
+        if ($check_new_data) {
                 /*
                  * Should add some kind of check to make sure the wiki summaries are available on the server.
                  * Should also make this server driven by the link table.
@@ -126,44 +142,43 @@ class talkwikitome {
         			$feedURL .= '|'.$tags[$i]['tag'];
         		} 
         	}
-        	
-        	//error_log("wiki feed URL: ".$feedURL);
+        	error_log("wiki feed URL: ".$feedURL);
         	
         	$urlHandle = fopen($feedURL, "r");
         	if ($urlHandle) {
         		$jsonData = stream_get_contents($urlHandle);
-				fclose($urlHandle);
-				$data = json_decode($jsonData, true);
-				$summaries = $data['summaries'];
-				$leftRight = "float: left;";
-				for ($i = 0; $i < count($tags); $i++) {
-					$tagName = html_entity_decode(rawurldecode(str_replace('_', ' ', $tags[$i]['tag'])));
-					$summary = $this->findWithin($summaries, $tagName);
-					error_log("Summary for: ".$tagName.': '.$summary);
-					if (strlen($summary['summary'])) {
-						$replacement = '<div class="wiki-info-sidebar">
-									<div class="wiki-info-sidebar-header">
-								        	<h3><a href="'.$summary['link'].'">'.$summary['title'].'</a></h3>
-									</div>
-									<div class="wiki-info-sidebar-body">
-										'.trim($summary['summary']).'<a href="'.$summary['link'].'" class="wiki-info-sidebar-and">...</a>
-									</div>
-								</div>';
+                        fclose($urlHandle);
+                        $data = json_decode($jsonData, true);
+                        $summaries = $data['summaries'];
+                        update_post_meta($post->ID, 'talkwikitome_wiki_summaries', $summaries);
+                }
+        }
+
+        $leftRight = "float: left;";
+        for ($i = 0; $i < count($summaries); $i++) {
+                $summary = $summaries[$i];
+                if (strlen($summary['summary'])) {
+                        $replacement = '<div class="wiki-info-sidebar">
+                                          <div class="wiki-info-sidebar-header">
+                                            <h3><a href="'.$summary['link'].'">'.$summary['title'].'</a></h3>
+                                          </div>
+                                          <div class="wiki-info-sidebar-body">
+                                            '.trim($summary['summary']).'<a href="'.$summary['link'].'" class="wiki-info-sidebar-and">...</a>
+                                          </div>
+                                        </div>';
 										
-						$pos = strpos($content, '/wiki/index.php/'.$tags[$i]['tag']);
-						$nextPos = strpos($content, "</p>", $pos);
-						if ($nextPos > $pos) {
-							$lastPos = strpos($content, "</p>", $nextPos+1);
-							if ($lastPos > $nextPos) {
-								$pos = $lastPos;
-							} else {
-								$pos = $nextPos;
-							} 
-						}
-						$content = substr_replace($content, '</p>'.$replacement, $pos, 4);
-					}
-				}
-        	}
+                        $pos = strpos($content, '/wiki/index.php/'.$tags[$i]['tag']);
+                        $nextPos = strpos($content, "</p>", $pos);
+                        if ($nextPos > $pos) {
+                                $lastPos = strpos($content, "</p>", $nextPos+1);
+                                        if ($lastPos > $nextPos) {
+                                                $pos = $lastPos;
+                                        } else {
+                                                $pos = $nextPos;
+                                        } 
+                        }
+                        $content = substr_replace($content, '</p>'.$replacement, $pos, 4);
+                }
         }
 
         return $content;
@@ -171,7 +186,6 @@ class talkwikitome {
     
     function findWithin($list, $tag) {
     	for ($i = 0; $i < count($list); $i++) {
-    		//error_log($list[$i]['title']);
     		if ($list[$i]['title'] == $tag) {
     			return $list[$i];
     		}
